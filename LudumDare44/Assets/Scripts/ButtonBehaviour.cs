77e569cf20000ee0bc352ceeb49299cf1f3808f5
Sebastian Fieber <fieber@gmx.de>
2019-04-27 21:46:59 +0200

    Button mit Animation

---
+++ LudumDare44/Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonBehaviour : MonoBehaviour {
+
+	private Animator animator;
+
+	void Awake()
+	{
+		animator = GetComponentInParent<Animator> ();
+		animator.SetBool ("isPressed", false);
+	}
+
+
+	void Start()
+	{
+		Physics.IgnoreCollision (transform.parent.GetComponent<Collider>(), GetComponent<Collider>());
+	}
+
+
+	// Use this for initialization
+	void OnCollisionEnter (Collision col) {
+	
+		if (col.gameObject.name == "Player Capsule") 
+		{
+			animator.SetTrigger("isPressed");
+		}
+	}
+
+	void OnCollisionStay (Collision col) 
+	{
+		foreach (ContactPoint contact in col.contacts) {
+						Debug.DrawRay (contact.point, contact.normal * 10, Color.white);
+				}
+	}
+
+	void OnCollisionExit (Collision col) {
+		
+		if (col.gameObject.name == "Player Capsule") 
+		{
+			//animator.SetTrigger("isPressed");
+			animator.SetBool ("isPressed", false);
+		}
+	}
+
+}
---
+++ LudumDare44/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+
+
+
+    void Awake()
+    {
+        //Time.
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
