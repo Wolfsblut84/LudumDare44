9fa1495b01034e1827a4f46e5417041c5003c4ef
Sebastian Fieber <fieber@gmx.de>
2019-04-29 13:06:15 +0200

    Add Main Menu and load Level 1 on Start Button

---
+++ LudumDare44/Assets/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private float timer = 1.0f;
+    private float waitingTime = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > waitingTime)
+        {
+            //Action
+            ShootAt(target.transform.position);// + new Vector3(Random.Range(-2, 2), Random.Range(-2, 2), Random.Range(-2, 2)));
+            timer = 0;
+        }
+
+    }
+
+
+
+    public void ShootAt(Vector3 targetPosition)
+    {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+    }
+
+}
